130521be0423c438c85da5d960300f4ce18ef16e
Forairaaaaa <applesyqd@outlook.com>
2023-08-16 15:14:23 +0800

    .

--- leetcode/easy.cpp
+++ leetcode/easy.cpp
@@ -18,44 +18,19 @@ std::vector<int> EASY::twoSum(std::vector<int>& nums, int target)
     dbg(nums);
     std::vector<int> result;
 
-
-    struct Sum_t
-    {
-        int index_1;
-        int index_2;
-        int sum;
-    };
-
-
-    /* Get a sum up list */
-    std::vector<Sum_t> sum_list;
-    Sum_t sum_buffer;
+    /* Iterate the shit out */
     for (int i = 0; i < (nums.size() - 1); i++)
     {
         for (int j = (i + 1); j < nums.size(); j++)
         {
-            printf("[%d][%d]\n", i, j);
-
-            sum_buffer.index_1 = i;
-            sum_buffer.index_2 = j;
-            sum_buffer.sum = nums[i] + nums[j];
-            sum_list.emplace_back(sum_buffer);
+            if ((nums[i] + nums[j]) == target)
+            {
+                result.push_back(i);
+                result.push_back(j);
+                break;
+            }
         }
     }
-    
-
-
-    /* Match target */
-    for (const auto& i : sum_list)
-    {
-        if (i.sum == target)
-        {
-            result.emplace_back(i.index_1);
-            result.emplace_back(i.index_2);
-            break;
-        }
-    }
-    
 
     return result;
 }
