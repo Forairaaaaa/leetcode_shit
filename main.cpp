e4ffe88407bdb9f4b5e6741f54bdc323cab428d5
Forairaaaaa <applesyqd@outlook.com>
2024-01-31 17:31:05 +0800

    [Add Binary] https://leetcode.com/problems/add-binary/description/

--- main.cpp
+++ main.cpp
@@ -8,9 +8,12 @@
  * @copyright Copyright (c) 2023
  *
  */
+#include <algorithm>
 #include <benchmark/benchmark.h>
+#include <cmath>
 #include <cstdio>
 #include <iostream>
+#include <string>
 
 // #define GO_BENCHMARK
 
@@ -19,18 +22,91 @@
 #endif
 #include <dbg.h>
 
-
 /* -------------------------------------------------------------------------- */
 /*                                 Normal shit                                */
 /* -------------------------------------------------------------------------- */
+using namespace std;
+string addBinary(string a, string b)
+{
+    string result;
+
+    bool carry = false;
+    bool bit_a = false;
+    bool bit_b = false;
+    int bit_on_num = 0;
+
+    int max_len = 0;
+    if (a.size() > b.size())
+        max_len = a.size();
+    else
+        max_len = b.size();
+
+    for (int index = 0; index < max_len + 1; index++)
+    {
+        dbg("---------------------");
+        dbg(index);
+
+        // Get bit
+        if (index < a.size())
+            bit_a = a[a.size() - index - 1] == '1';
+        else
+            bit_a = false;
+
+        if (index < b.size())
+            bit_b = b[b.size() - index - 1] == '1';
+        else
+            bit_b = false;
+
+        dbg(bit_a);
+        dbg(bit_b);
+        dbg(carry);
+
+        // Count 1 num
+        bit_on_num = 0;
+        if (bit_a)
+            bit_on_num++;
+        if (bit_b)
+            bit_on_num++;
+        if (carry)
+            bit_on_num++;
+        dbg(bit_on_num);
+
+        // Add
+        if (bit_on_num == 0)
+        {
+            result.insert(result.begin(), '0');
+            carry = false;
+        }
+        else if (bit_on_num == 1)
+        {
+            result.insert(result.begin(), '1');
+            carry = false;
+        }
+        else if (bit_on_num == 2)
+        {
+            result.insert(result.begin(), '0');
+            carry = true;
+        }
+        else if (bit_on_num == 3)
+        {
+            result.insert(result.begin(), '1');
+            carry = true;
+        }
+    }
+
+    dbg(result);
+    return result;
+}
+
 #ifndef GO_BENCHMARK
 int main()
 {
-    dbg("????");
+    addBinary("11", "1");
+    addBinary("11", "001");
+    addBinary("1010", "1011");
     return 0;
 }
 #endif
-
 
 /* -------------------------------------------------------------------------- */
 /*                                  Benchmark                                 */
